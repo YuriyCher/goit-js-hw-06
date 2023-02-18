ad8cd7612be7f947313293daa13de9f2ccbb45cb
y0979441322@ukr.net <y0979441322@ukr.net>
2023-02-18 22:45:42 +0000

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,23 @@
+const listWithId = document.querySelector('#menu');
+listWithId.style.textTransform = 'uppercase';
+listWithId.style.fontSize = '24px';
+console.log(listWithId);
 
-
-
-
+<ul>
+  <li>First item</li>
+  <li>Second item</li>
+  <li>Third item</li>
+  <li>Fourth item</li>
+  <li>Fifth item</li>
+</ul>
+document
+<ul>
+    <li>
 // Напиши скрипт, який:
 
 // Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку
+ елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
 // В результаті, в консолі будуть виведені наступні повідомлення.
 
 // Number of categories: 3
